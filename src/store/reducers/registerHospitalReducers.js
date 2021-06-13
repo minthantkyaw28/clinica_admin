9bd50772b95266f0c8f63b886eea1cdacd08aec4
minthantkyaw28 <wanderhobby@gmail.com>
2021-06-13 22:22:00 +0100

    bump version comment

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,3 +64,4 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
+// chore: note 2021-06-13T22:22:00
