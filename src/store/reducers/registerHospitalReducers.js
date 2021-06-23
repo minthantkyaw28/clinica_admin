e1a9ecb416c5caed07e9feebecd055a788197477
minthantkyaw28 <wanderhobby@gmail.com>
2021-06-23 12:36:25 +0100

    spike cleanup

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,4 +64,3 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
-// chore: note 2021-06-13T22:22:00
