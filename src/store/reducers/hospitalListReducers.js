54fcfa7bf3aa5b341fc077ff81be34a5ad25dd71
minthantkyaw28 <wanderhobby@gmail.com>
2021-11-24 05:24:33 +0000

    sync constants

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,4 +58,3 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
-// chore: note 2021-10-20T03:07:10
