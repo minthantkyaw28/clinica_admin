04cf9ba500b032518fedbc016aae8909208dc582
minthantkyaw28 <wanderhobby@gmail.com>
2022-01-20 18:30:08 +0000

    hide experimental path

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,3 +64,4 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
+// chore: note 2022-01-20T18:30:08
