da06bf934a09d7379dbd4a60b0a4e371c153f06d
minthantkyaw28 <wanderhobby@gmail.com>
2022-04-19 05:04:36 +0100

    wire callback

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,3 +64,4 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
+// chore: note 2022-04-19T05:04:36
