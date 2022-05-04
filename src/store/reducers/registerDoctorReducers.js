27861774046dbed82adbc96f13e51c05db6212e8
minthantkyaw28 <wanderhobby@gmail.com>
2022-05-04 14:50:11 +0100

    sync constants

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2022-05-04T14:50:11
