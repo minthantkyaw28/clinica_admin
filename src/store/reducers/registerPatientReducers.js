28d32a4b2f962ed7c5d1c730d0c75177365eeb86
minthantkyaw28 <wanderhobby@gmail.com>
2022-09-29 15:11:46 +0100

    fix lint warning

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,3 +64,4 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
+// chore: note 2022-09-29T15:11:46
