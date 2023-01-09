c7a6d54bf4619731526f9bac1185d453940728ea
minthantkyaw28 <wanderhobby@gmail.com>
2023-01-09 18:50:27 +0000

    build: tweak script

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,3 +64,4 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
+// chore: note 2023-01-09T18:50:27
