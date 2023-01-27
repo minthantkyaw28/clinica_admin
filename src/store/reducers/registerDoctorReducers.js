8075b2334582236a449708e6325a8940f17c7e45
minthantkyaw28 <wanderhobby@gmail.com>
2023-01-27 21:08:14 +0000

    debounce handler

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,4 +65,3 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
-// chore: note 2022-10-10T04:17:42
