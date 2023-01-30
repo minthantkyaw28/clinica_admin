81f1afc616405dda44ba4b7c9c3255a72245b293
minthantkyaw28 <wanderhobby@gmail.com>
2023-01-30 15:43:44 +0000

    refactor: slice module

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,4 +58,3 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
-// chore: note 2022-10-18T04:13:55
