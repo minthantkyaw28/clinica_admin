ba90e317ed365d758ed2b4894d6b9c6235e9a7e4
minthantkyaw28 <wanderhobby@gmail.com>
2023-02-06 19:45:05 +0000

    spike cleanup

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,3 +58,4 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
+// chore: note 2023-02-06T19:45:05
