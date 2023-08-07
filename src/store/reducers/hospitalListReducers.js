2db5a376231608fdd110f72cc4618b4c4fe14d90
minthantkyaw28 <wanderhobby@gmail.com>
2023-08-07 15:46:01 +0100

    align naming

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,3 +58,4 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
+// chore: note 2023-08-07T15:46:01
