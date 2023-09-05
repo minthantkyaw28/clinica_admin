adf3b895ac514f4ffc94de5f64d2939641d734b6
minthantkyaw28 <wanderhobby@gmail.com>
2023-09-05 06:56:32 +0100

    simplify logic

--- src/store/reducers/hospitalTransactionSingleDayReducers.js
+++ src/store/reducers/hospitalTransactionSingleDayReducers.js
@@ -66,4 +66,3 @@ export const hospitalTransactionSingleDayActions =
   hospitalTransactionSingleDaySlice.actions;
 
 export default hospitalTransactionSingleDaySlice.reducer;
-// chore: note 2023-08-28T04:43:50
