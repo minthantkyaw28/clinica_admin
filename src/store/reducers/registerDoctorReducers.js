2ff5987da87d56a6b98dceff3e532180b72d5d32
minthantkyaw28 <wanderhobby@gmail.com>
2023-11-04 10:43:46 +0000

    bump version comment

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2023-11-04T10:43:46
