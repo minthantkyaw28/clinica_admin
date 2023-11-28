9cc4359bc43362778d9b9a378be4be1024da0837
minthantkyaw28 <wanderhobby@gmail.com>
2023-11-28 12:20:26 +0000

    style: nits

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2023-11-28T12:20:26
