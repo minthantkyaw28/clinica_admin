21f2144769a799358d13577bb10bc0280909a129
minthantkyaw28 <wanderhobby@gmail.com>
2024-04-15 05:18:25 +0100

    reorder blocks

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,4 +64,3 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
-// chore: note 2024-02-10T09:05:34
