648c87779559121de5349d564ac1cc1415487d4f
minthantkyaw28 <wanderhobby@gmail.com>
2024-07-16 08:10:27 +0100

    wire callback

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,4 +65,3 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
-// chore: note 2024-02-13T04:46:05
