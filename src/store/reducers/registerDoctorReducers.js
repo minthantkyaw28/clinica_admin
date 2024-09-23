30385d20f1e9de538e5d6c26c08d2def880650d2
minthantkyaw28 <wanderhobby@gmail.com>
2024-09-23 13:11:29 +0100

    polish docs

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2024-09-23T13:11:29
