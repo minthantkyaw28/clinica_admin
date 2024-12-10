953cac7b7264944ec97ebd03ac33ddf0bac5fd26
minthantkyaw28 <wanderhobby@gmail.com>
2024-12-10 12:49:16 +0000

    sort imports

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,4 +64,3 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
-// chore: note 2024-05-14T03:29:07
