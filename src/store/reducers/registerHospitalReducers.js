7266d1abe1fda9af82caa50340fba84e21b99b34
minthantkyaw28 <wanderhobby@gmail.com>
2024-12-20 16:36:52 +0000

    harden validation

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,3 +64,4 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
+// chore: note 2024-12-20T16:36:52
