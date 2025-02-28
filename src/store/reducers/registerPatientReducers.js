568b306d8c29e329b67a07a883ba822e03142c44
minthantkyaw28 <wanderhobby@gmail.com>
2025-02-28 13:08:38 +0000

    stub new handler

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,3 +64,4 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
+// chore: note 2025-02-28T13:08:38
