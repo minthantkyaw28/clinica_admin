65dafa015c2fefdf40c224d2c6d2e4c80dd7eb3b
minthantkyaw28 <wanderhobby@gmail.com>
2025-07-28 11:04:44 +0100

    hide experimental path

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,4 +64,3 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
-// chore: note 2025-02-28T13:08:38
