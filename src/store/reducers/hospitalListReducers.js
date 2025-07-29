cebf3949e781ed0633beb9e205aae73e0f7c2b58
minthantkyaw28 <wanderhobby@gmail.com>
2025-07-29 17:29:45 +0100

    fix lint warning

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,4 +58,3 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
-// chore: note 2025-07-04T08:46:32
