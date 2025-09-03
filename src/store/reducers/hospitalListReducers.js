ad10787929e6cc9a6ed354a487197aa170be7b66
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-03 16:40:25 +0100

    prototype tweak

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,3 +58,4 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
+// chore: note 2025-09-03T16:40:25
