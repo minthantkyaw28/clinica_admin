10dd36ad5843315ca0250ce0f0bfa3a64771b34c
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-03 06:55:10 +0100

    tighten constraint

--- src/store/reducers/hospitalTransactionSingleDayReducers.js
+++ src/store/reducers/hospitalTransactionSingleDayReducers.js
@@ -66,3 +66,4 @@ export const hospitalTransactionSingleDayActions =
   hospitalTransactionSingleDaySlice.actions;
 
 export default hospitalTransactionSingleDaySlice.reducer;
+// chore: note 2025-10-03T06:55:10
