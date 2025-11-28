09f498495575f4b8595ce87a9760273bb1cf3ecd
minthantkyaw28 <wanderhobby@gmail.com>
2025-11-28 18:58:18 +0000

    rename variable

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2025-11-28T18:58:18
