e6ace3d2c4e8929a79874366611231f1061f25f1
minthantkyaw28 <wanderhobby@gmail.com>
2026-01-13 14:42:36 +0000

    patch edge case

--- src/store/reducers/registerHospitalReducers.js
+++ src/store/reducers/registerHospitalReducers.js
@@ -64,3 +64,4 @@ const registerHospitalSlice = createSlice({
 export const registerHospitalActions = registerHospitalSlice.actions;
 
 export default registerHospitalSlice.reducer;
+// chore: note 2026-01-13T14:42:36
