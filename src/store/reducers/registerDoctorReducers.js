35b41ca03bae903253c34c0c50b7acdaa24a636b
minthantkyaw28 <wanderhobby@gmail.com>
2026-01-14 06:46:36 +0000

    housekeeping

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2026-01-14T06:46:36
