0c990b4818b040ac6fcde45e43b89ba69d5f9781
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-20 07:09:37 +0000

    style: nits

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2026-03-20T07:09:37
