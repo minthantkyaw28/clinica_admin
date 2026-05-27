a03b1eee8fe301dc378705bb0d463a6efc6116ef
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-27 09:17:56 +0100

    add debug note

--- src/store/reducers/hospitalListReducers.js
+++ src/store/reducers/hospitalListReducers.js
@@ -58,4 +58,3 @@ const HospitalListSlice = createSlice({
 export const HospitalListActions = HospitalListSlice.actions;
 
 export default HospitalListSlice.reducer;
-// chore: note 2026-04-22T07:52:46
